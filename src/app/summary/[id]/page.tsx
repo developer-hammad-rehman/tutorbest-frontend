2b08c0a84ff876566958e2cb3cfabfb77f5208cb
Hammad-Rehman <124163466+developer-hammad-rehman@users.noreply.github.com>
2024-04-10 18:15:53 +0500

    main

--- src/app/summary/[id]/page.tsx
+++ src/app/summary/[id]/page.tsx
@@ -22,7 +22,7 @@ export default function Summary({params} : {params : {id : number}}) {
   return (
        <div className='lg:px-10'>
          <div  className='bg-white border-2 border-gray-100 rounded-2xl py-7 px-6'>
-          <h1 className='text-2xl font-bold border-b-2 border-gray-500'>{title}"</h1>
+          <h1 className='text-2xl font-bold border-b-2 border-gray-500'>{title}&ldquo;</h1>
        <Suspense fallback={<div>Loading...</div>}><p className='h-80 overflow-y-scroll overflow-x-hidden'>{data?data[0].summary:null}</p></Suspense>
         </div>
        </div>

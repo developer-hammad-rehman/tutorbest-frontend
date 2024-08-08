3c8dae82606f699cf0fd234a3c70872b996e13d8
Hammad-Rehman <124163466+developer-hammad-rehman@users.noreply.github.com>
2024-08-08 23:58:16 +0500

    changes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
   } else {
     return (
       <>
-        <div className="border-t-2 border-gray-800 flex flex-col lg:flex-row w-full items-center justify-between  px-5 gap-4 bg-gradient-to-tr from-[#2D155E] to-black py-4">
+        <div className="border-t-2 border-gray-800 flex flex-col w-full items-center justify-between  px-5 gap-4 bg-gradient-to-tr from-[#2D155E] to-black py-4">
           <div className="text-white">
             <Image
               src={logo}
@@ -23,31 +23,8 @@ export default function Footer() {
               onClick={() => router.push("/")}
             />
           </div>
-          <div>
-            <h5 className="text-xl font-bold text-gray-50">Website</h5>
-            <ul className="flex flex-col gap-2 justify-center items-center text-gray-400">
-              <Link href={"/sigin"}>Login</Link>
-              <Link href={"/sigup"}>Sign Up</Link>
-            </ul>
-          </div>
-          <div>
-            <h5 className="text-xl font-bold text-gray-50">SOCIAL MEDIA</h5>
-            <ul className="flex flex-col gap-2 justify-center items-center text-gray-400">
-              <Link href={"https://www.instagram.com"}>Instagram</Link>
-              <Link href={"https://www.facebook.com"}>Facebook</Link>
-              <Link href={"https://www.linkedin.com"}>LinkedIn</Link>
-            </ul>
-          </div>
-          <div>
-            <h5 className="text-xl font-bold text-gray-50">PAGES</h5>
-            <ul className="flex flex-col gap-2 justify-center items-center text-gray-400">
-              <Link href={"#"}>Tools</Link>
-              <Link href={"/privicay-policy"}>Privacy</Link>
-              <Link href={"/privicay-policy"}>Terms</Link>
-            </ul>
-          </div>
         </div>
-        <div className="bg-black text-gray-400 text-sm py-2 px-4 flex justify-between items-center">
+        <div className="bg-black text-gray-400 text-center text-xl lg:text-2xl py-2 px-4 flex justify-between items-center">
           <span>Nextrend, LLC 1111B S Governors Ave #20753 Dover, DE 19904</span>
           <span>001 (302) 310-4370</span>
         </div>
